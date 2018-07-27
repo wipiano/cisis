e1f88f6d26fc647988607287ac040c2b5e95ee6b
Kohei Hakoishi <wipiano@gmail.com>
2018-07-27 18:04:57 +0900

    add Nullable<T>.TryGetValue()

---
+++ Cisis/NullableExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Cisis
+{
+    public static class NullableExtensions
+    {
+        public static bool TryGetValue<T>(this T? nullable, out T value)
+            where T : struct
+        {
+            value = nullable ?? default;
+            return nullable.HasValue;
+        }
+    }
+}
