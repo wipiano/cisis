a1c8fabd905af5d7b50fc708eebdf76e4592c0e0
Kohei Hakoishi <wipiano@gmail.com>
2018-07-27 20:51:42 +0900

    test: NullableExtensions

---
+++ Cisis.Test/NullableTest.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace Cisis.Test
+{
+    public class NullableTest
+    {
+        [Theory]
+        [InlineData(1, 1, true)]
+        [InlineData(null, 0, false)]
+        public void TryGetValueTest(int? nullable, int expectedValue, bool expectedReturn)
+        {
+            nullable.TryGetValue(out int value).Is(expectedReturn);
+            value.Is(expectedValue);
+        }
+    }
+}
--- Cisis/NullableExtensions.cs
+++ Cisis/NullableExtensions.cs
@@ -1,7 +1,11 @@
-﻿namespace Cisis
+﻿using System;
+namespace Cisis
 {
     public static class NullableExtensions
     {
+        /// <summary>
+        /// Attempts to get the value from <see cref="Nullable{T}"/>
+        /// </summary>
         public static bool TryGetValue<T>(this T? nullable, out T value)
             where T : struct
         {
