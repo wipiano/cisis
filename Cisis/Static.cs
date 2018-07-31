5b700156aae946953c289c96432ed919c3a6422e
Kohei Hakoishi <wipiano@gmail.com>
2018-08-01 00:05:12 +0900

    Null()

---
+++ Cisis/Static.cs
@@ -0,0 +1,15 @@
+﻿namespace Cisis
+{
+    /// <summary>
+    /// Helper functions
+    /// </summary>
+    public static class Static
+    {
+        /// <summary>
+        /// return null as T
+        /// </summary>
+        public static T Null<T>()
+            where T : class
+            => null;
+    }
+}
