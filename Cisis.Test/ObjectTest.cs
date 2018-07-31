29f8e42f75c627bc7c274c8dc0b677cea667b3e4
Kohei Hakoishi <wipiano@gmail.com>
2018-08-01 00:32:53 +0900

    Map

---
+++ Cisis.Test/ObjectTest.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Cisis.Test
+{
+    public class ObjectTest
+    {
+        [Fact]
+        public void MapTest()
+        {
+            1.Map(x => x * 2).Is(2);
+        }
+    }
+}
---
+++ Cisis/ObjectExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Cisis
+{
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// Map T1 to T2
+        /// </summary>
+        public static T2 Map<T1, T2>(this T1 v, Func<T1, T2> mapper)
+            => mapper(v);
+    }
+}
