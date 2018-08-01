9f55a3bfbc35adf807835b890a5f9851cfcde930
Kohei Hakoishi <wipiano@gmail.com>
2018-08-01 23:38:02 +0900

    IEnumerable.Split

---
+++ Cisis.Test/Linq/SplitTest.cs
@@ -0,0 +1,80 @@
+﻿using System.Linq;
+using Cisis.Linq;
+using Xunit;
+
+namespace Cisis.Test.Linq
+{
+    public class SplitTest
+    {
+        [Fact]
+        public void SmallSourceSplitTest()
+        {
+            var splitted = Enumerable.Range(0, 4).Split(1).ToArray();
+
+            splitted.Length.Is(4);
+
+            for (var i = 0; i < 4; i++)
+            {
+                var span = splitted[i].Span;
+                span.Length.Is(1);
+                span[0].Is(i);
+            }
+        }
+
+        [Fact]
+        public void SmallSourceSplitReferenceTypeTest()
+        {
+            var splitted = Enumerable.Range(0, 4).Select(x => new IntWrapper(x))
+                .Split(2).ToArray();
+            
+            splitted.Length.Is(2);
+
+            for (var i = 0; i < 2; i++)
+            {
+                for (var j = 0; j < 2; j++)
+                {
+                    splitted[i].Span[j].Value.Is(i * 2 + j);
+                }
+            }
+        }
+
+        [Fact]
+        public void NotSplittedTest()
+        {
+            var splitted = Enumerable.Range(0, 4).Split(4).ToArray();
+            
+            splitted.Length.Is(1);
+            splitted[0].Span.ToArray().Is(Enumerable.Range(0, 4));
+            
+            var splitted2 = Enumerable.Range(0, 4).Split(100).ToArray();
+            
+            splitted2.Length.Is(1);
+            splitted2[0].Span.ToArray().Is(Enumerable.Range(0, 4));
+        }
+
+        [Fact]
+        public void LargeSourceSplitTest()
+        {
+            var splitted = Enumerable.Range(0, 1000).Split(100).ToArray();
+            
+            splitted.Length.Is(10);
+
+            foreach (var block in splitted)
+            {
+                block.Span.Length.Is(100);
+            }
+            
+            splitted.SelectMany(s => s.Span.ToArray()).Is(Enumerable.Range(0, 1000));
+        }
+
+        public sealed class IntWrapper
+        {
+            public int Value { get; }
+
+            public IntWrapper(int value)
+            {
+                Value = value;
+            }
+        }
+    }
+}
---
+++ Cisis/Linq/Split.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cisis.Linq
+{
+    public static partial class LinqExtensions
+    {
+        public static IEnumerable<Memory<T>> Split<T>(this IEnumerable<T> source, int blockSize)
+        {
+            IEnumerable<Memory<T>> EnumerateInner()
+            {
+
+                using (var e = source.GetEnumerator())
+                {
+                    var array = new T[blockSize];
+                    var i = 0;
+                    
+                    while (e.MoveNext())
+                    {
+                        array[i++] = e.Current;
+
+                        if (i == blockSize)
+                        {
+                            yield return new Memory<T>(array);
+                            i = 0;
+                            array = new T[blockSize];
+                        }
+                    }
+
+                    if (i > 0)
+                    {
+                        yield return new Memory<T>(array, 0, i);
+                    }
+                }
+            }
+
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (blockSize < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(blockSize));
+            }
+
+            return EnumerateInner();
+        }
+    }
+}
