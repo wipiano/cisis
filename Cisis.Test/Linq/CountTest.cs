0ba1b9e6362ec81ecf7b97d1fbaac6e9bdf7e506
Kohei Hakoishi <wipiano@gmail.com>
2018-08-08 23:29:10 +0900

    CountIfPossible

---
+++ Cisis.Test/Linq/CountTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using Cisis.Linq;
+using Xunit;
+
+namespace Cisis.Test.Linq
+{
+    public class CountTest
+    {
+        [Fact]
+        public void CountIfPossibleTest()
+        {
+            new int[3].CountIfPossible().Is(3);
+
+            Enumerable.Range(0, 4).Where(x => x < 2).CountIfPossible().IsNull();
+        }       
+    }
+}
---
+++ Cisis/Linq/Count.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Cisis.Linq
+{
+    public static partial class LinqExtensions
+    {
+        /// <summary>
+        /// Get count if source is <see cref="ICollection{T}"/>
+        /// </summary>
+        public static int? CountIfPossible<T>(this IEnumerable<T> source)
+        {
+            source.ThrowIfArgumentNull(nameof(source));
+
+            if (source is ICollection<T> c) return c.Count;
+
+            return null;
+        }
+    }
+}
