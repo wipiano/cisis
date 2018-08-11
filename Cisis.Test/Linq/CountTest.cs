fbae3caaea835713535ea568e4cc731f7866d42e
Kohei Hakoishi <wipiano@gmail.com>
2018-08-11 23:53:35 +0900

    CountLt, CountLe, CountGe, CountGt

--- Cisis.Test/Linq/CountTest.cs
+++ Cisis.Test/Linq/CountTest.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using Cisis.Linq;
 using Xunit;
+using static Cisis.Static;
 
 namespace Cisis.Test.Linq
 {
@@ -12,6 +13,46 @@ namespace Cisis.Test.Linq
             new int[3].CountIfPossible().Is(3);
 
             Enumerable.Range(0, 4).Where(x => x < 2).CountIfPossible().IsNull();
-        }       
+        }
+
+        [Theory]
+        [InlineData(1, 2, true)]
+        [InlineData(2, 2, false)]
+        [InlineData(3, 2, false)]
+        public void CountLtTest(int sourceLength, int maxLength, bool expected)
+        {
+            Range(0, sourceLength).CountLt(maxLength).Is(expected);
+            new int[sourceLength].CountLt(maxLength).Is(expected);
+        }
+
+        [Theory]
+        [InlineData(1, 2, true)]
+        [InlineData(2, 2, true)]
+        [InlineData(3, 2, false)]
+        public void CountLeTest(int sourceLength, int maxLength, bool expected)
+        {
+            Range(0, sourceLength).CountLe(maxLength).Is(expected);
+            new int[sourceLength].CountLe(maxLength).Is(expected);
+        }
+        
+        [Theory]
+        [InlineData(1, 2, false)]
+        [InlineData(2, 2, false)]
+        [InlineData(3, 2, true)]
+        public void CountGtTest(int sourceLength, int minLength, bool expected)
+        {
+            Range(0, sourceLength).CountGt(minLength).Is(expected);
+            new int[sourceLength].CountGt(minLength).Is(expected);
+        }
+        
+        [Theory]
+        [InlineData(1, 2, false)]
+        [InlineData(2, 2, true)]
+        [InlineData(3, 2, true)]
+        public void CountGeTest(int sourceLength, int minLength, bool expected)
+        {
+            Range(0, sourceLength).CountGe(minLength).Is(expected);
+            new int[sourceLength].CountGe(minLength).Is(expected);
+        }
     }
 }
--- Cisis/Linq/Count.cs
+++ Cisis/Linq/Count.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Cisis.Linq
 {
@@ -13,7 +14,149 @@ namespace Cisis.Linq
 
             if (source is ICollection<T> c) return c.Count;
 
+            if (source is IReadOnlyCollection<T> rc) return rc.Count;
+
             return null;
+        }
+
+        public static bool CountLt<T>(this IEnumerable<T> source, int value)
+        {
+            source.ThrowIfArgumentNull(nameof(source));
+            value.ThrowIfArgumentOutOfRange(1, int.MaxValue, nameof(value));
+
+            if (source.CountIfPossible().TryGetValue(out var c))
+            {
+                return c < value;
+            }
+
+            int count = 0;
+
+            using (var enumerator = source.GetEnumerator())
+            {
+                while (enumerator.MoveNext())
+                {
+                    count++;
+
+                    if (count >= value)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public static bool CountLe<T>(this IEnumerable<T> source, int value)
+        {
+            source.ThrowIfArgumentNull(nameof(source));
+            value.ThrowIfArgumentOutOfRange(0, int.MaxValue, nameof(value));
+
+            if (source.CountIfPossible().TryGetValue(out var c))
+            {
+                return c <= value;
+            }
+
+            int count = 0;
+
+            using (var enumerator = source.GetEnumerator())
+            {
+                while (enumerator.MoveNext())
+                {
+                    count++;
+
+                    if (count > value)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public static bool CountGt<T>(this IEnumerable<T> source, int value)
+        {
+            source.ThrowIfArgumentNull(nameof(source));
+            value.ThrowIfArgumentOutOfRange(0, int.MaxValue, nameof(value));
+
+            if (source.CountIfPossible().TryGetValue(out var c))
+            {
+                return c > value;
+            }
+
+            int count = 0;
+
+            using (var enumerator = source.GetEnumerator())
+            {
+                while (enumerator.MoveNext())
+                {
+                    count++;
+
+                    if (count > value)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public static bool CountGe<T>(this IEnumerable<T> source, int value)
+        {
+            source.ThrowIfArgumentNull(nameof(source));
+            value.ThrowIfArgumentOutOfRange(0, int.MaxValue, nameof(value));
+
+            if (source.CountIfPossible().TryGetValue(out var c))
+            {
+                return c >= value;
+            }
+
+            int count = 0;
+
+            using (var enumerator = source.GetEnumerator())
+            {
+                while (enumerator.MoveNext())
+                {
+                    count++;
+
+                    if (count >= value)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public static bool CountEq<T>(this IEnumerable<T> source, int value)
+        {
+            source.ThrowIfArgumentNull(nameof(source));
+            value.ThrowIfArgumentOutOfRange(0, int.MaxValue, nameof(value));
+
+            if (source.CountIfPossible().TryGetValue(out var c))
+            {
+                return c >= value;
+            }
+
+            int count = 0;
+
+            using (var enumerator = source.GetEnumerator())
+            {
+                while (enumerator.MoveNext())
+                {
+                    count++;
+
+                    if (count > value)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return count == value;
         }
     }
 }
